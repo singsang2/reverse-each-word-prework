3f9088feceeac7d6dd22fa8672692d5e8978cc9d
Sung Bae <singsang2@gmail.com>
2018-10-06 21:22:52 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
 def reverse_each_word(string)
   word_list = string.split(" ")
-  word_list.collect do |word|
+  new_list = word_list.collect do |word|
     word = word.reverse
   end
+  new_list.join(" ")
 end
   
